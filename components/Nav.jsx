a0f80008b211efcf7ac9d96ea3d499062295302c
Loïc Torres <loic.torresg@gmail.com>
2022-06-01 14:47:03 +0200

    [fix] nav light

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -24,15 +24,13 @@ const Nav = ({ children }) => {
         });
       }
 
-      if (
-        window.location.href === "http://localhost:3000/Layouts/lightSidenav"
-      ) {
+      if (window.location.href === `${window.origin}/Layouts/lightSidenav`) {
         if (sidenavAccordion) {
           sidenavAccordion.className = "sb-sidenav accordion sb-sidenav-light";
         }
       }
     };
-  }, []);
+  });
 
   return (
     <>
