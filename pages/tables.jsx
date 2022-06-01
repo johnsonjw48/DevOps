a361d715382a53d0ae883f0033486c67ef52f8a3
Loïc Torres <loic.torresg@gmail.com>
2022-06-01 14:26:51 +0200

    [fix] breadcrumb navigation

--- pages/tables.jsx
+++ pages/tables.jsx
@@ -1,32 +1,34 @@
 import Datatable from "../components/Datatable";
 
 const Tables = () => {
-    return (
-        <div>
-            <div className="container-fluid px-4">
-                            <h1 class="mt-4">Tables</h1>
-                            <ol class="breadcrumb mb-4">
-                                <li class="breadcrumb-item"><a href="index.html">Dashboard</a></li>
-                                <li class="breadcrumb-item active">Tables</li>
-                            </ol>
-                            <div class="card mb-4">
-                                <div class="card-body">
-                                    DataTables is a third party plugin that is used to generate the demo table below. For more information about DataTables, please visit the
-                                    <a target="_blank" href="https://datatables.net/"> official DataTables documentation</a>
-                                    .
-                                </div>
-                            </div>
-    
-            </div>
-
-
-        <div>
-            <Datatable></Datatable>
+  return (
+    <div>
+      <div className="container-fluid px-4">
+        <h1 className="mt-4">Tables</h1>
+        <ol className="breadcrumb mb-4">
+          <li className="breadcrumb-item">
+            <a href="/">Dashboard</a>
+          </li>
+          <li className="breadcrumb-item active">Tables</li>
+        </ol>
+        <div className="card mb-4">
+          <div className="card-body">
+            DataTables is a third party plugin that is used to generate the demo
+            table below. For more information about DataTables, please visit the
+            <a target="_blank" href="https://datatables.net/" rel="noreferrer">
+              {" "}
+              official DataTables documentation
+            </a>
+            .
+          </div>
         </div>
-        </div>
-      
-    );
-  };
-  
-  export default Tables;
-  
+      </div>
+
+      <div>
+        <Datatable></Datatable>
+      </div>
+    </div>
+  );
+};
+
+export default Tables;
