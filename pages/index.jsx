4caa7e2402d247553a4f6befcdfdf587f674530b
Loïc Torres <loic.torresg@gmail.com>
2022-06-01 10:01:28 +0200

    [feature] added cards and datatable

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,6 +1,8 @@
 import styles from "../styles/Home.module.css";
 import Datatable from "../components/Datatable";
 import Card from "../components/Card";
+import Chart from "../components/Chart";
+import Box from "@mui/material/Box";
 
 const Home = () => {
   return (
@@ -9,8 +11,14 @@ const Home = () => {
         href="https://cdn.jsdelivr.net/npm/simple-datatables@latest/dist/style.css"
         rel="stylesheet"
       />
-      <Card color="success">Success Card</Card>
+      <div>
+        <Card color="primary">Primary Card</Card>
+        <Card color="secondary">Secondary Card</Card>
+        <Card color="success">Success Card</Card>
+        <Card color="danger">Danger Card</Card>
+      </div>
       <Datatable></Datatable>
+      {/* <Chart></Chart> */}
     </div>
   );
 };
